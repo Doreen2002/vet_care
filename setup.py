7be1dd1a6bc09c490f8c24f9ef28b1d39ec8d98e
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-09-01 20:37:23 +0200

    setup config

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import re, ast
 
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in vet_care/__init__.py
-from vet_care import __version__ as version
+
+# get version from __version__ variable in pos_bahrain/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('vet_care/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='vet_care',
@@ -18,3 +24,5 @@ setup(
 	include_package_data=True,
 	install_requires=install_requires
 )
+
+
