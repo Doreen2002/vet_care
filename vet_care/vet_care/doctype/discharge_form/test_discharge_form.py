464d39347ee49ae0745fd2a28a23d7ba62fc7571
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2025-09-03 13:56:27 +0300

    custom doctypes added

---
+++ vet_care/vet_care/doctype/discharge_form/test_discharge_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, 9T9IT and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDischargeForm(FrappeTestCase):
+	pass
---
+++ vet_care/vet_care/doctype/surgery_consent_form/surgery_consent_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, 9T9IT and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SurgeryConsentForm(Document):
+	pass
