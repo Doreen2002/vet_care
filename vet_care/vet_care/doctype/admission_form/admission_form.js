45a1466916d0284963e9662561cb26b056b28b53
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-09-24 15:22:31 +0200

    finalised fix for adimmsion form

--- vet_care/vet_care/doctype/admission_form/admission_form.js
+++ vet_care/vet_care/doctype/admission_form/admission_form.js
@@ -1,8 +1,75 @@
 // Copyright (c) 2025, 9T9IT and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Admission Form", {
-// 	refresh(frm) {
+frappe.ui.form.on("Admission Form", {
+	refresh(frm) {
+        frm.set_query('patient', () => {
+            return {
+                filters: {
+                    customer: frm.doc.client
+                }
+            }
+        })
+        frm.set_query('invoice_no', () => {
+            return {
+                filters: {
+                    customer: frm.doc.client
+                }
+            }
+        })
+        
 
-// 	},
-// });
+        frappe.db.get_doc('Vetcare Settings').then(doc => {
+            if(doc)
+            {
+                cur_frm.set_df_property("terms_and_conditions","options", doc.admission_form_text);
+                
+                cur_frm.refresh_fields();
+            }
+        });
+	},
+    patient(frm)
+    {
+        if(frm.doc.patient)
+        {
+            frappe.call({
+                method:"vet_care.vet_care.doctype.admission_form.admission_form.get_patient_details",
+                args:{'patient':frm.doc.patient},
+                callback: function(r) {
+                    if(r.message)
+                    {
+                        frm.set_value("breed", r.message.breed);
+                        frm.set_value("body_weight", r.message.body_wgt); 
+                        frm.set_value("gender", r.message.gender);
+                        frm.set_value("color", r.message.color);  
+                        frm.refresh_fields();
+
+                    }
+            }     });
+        }
+       
+    },
+    client(frm)
+    {
+        if(frm.doc.client)
+        {
+            frappe.call({
+                method:"vet_care.vet_care.doctype.admission_form.admission_form.get_customer_details",
+                args:{'customer':frm.doc.client},
+                callback: function(r) {
+                    if(r.message)
+                    {
+                        frm.set_value("address", r.message.address);
+                        frm.set_value("mobile", r.message.body_wgt); 
+                        frm.set_value("cpr", r.message.cpr);
+                        frm.set_value("email", r.message.email);
+                          
+                        frm.refresh_fields();
+
+                    }
+            }     });
+        }
+       
+    }
+    
+});
--- vet_care/vet_care/doctype/admission_form/admission_form.py
+++ vet_care/vet_care/doctype/admission_form/admission_form.py
@@ -1,9 +1,39 @@
 # Copyright (c) 2025, 9T9IT and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AdmissionForm(Document):
 	pass
+
+
+import json
+@frappe.whitelist()
+def get_patient_details(patient):
+	breed = ''
+	body_wgt = ''
+	gender = ''
+	color =''
+	patient = json.loads(patient)
+	for p in patient:
+		patient_doc = frappe.get_doc("Patient", p['patient'])
+		breed += patient_doc.vc_breed if patient_doc.vc_breed else ''
+		body_wgt +=str( patient_doc.vc_weight) if patient_doc.vc_weight else ''
+		gender += patient_doc.sex if patient_doc.sex else ''
+		color += patient_doc.vc_color if patient_doc.vc_color else ''
+	return {'breed':breed, 'body_wgt':body_wgt, 'gender': gender, 'color':color}
+
+@frappe.whitelist()
+def get_customer_details(customer):
+	address = ''
+	mobile_no = ''
+	cpr_no = ''
+	email =''
+	customer_doc = frappe.get_doc ('Customer', customer)
+	address = customer_doc.vc_flat_no if  customer_doc.vc_flat_no else ''  + customer_doc.vc_road_no if customer_doc.vc_road_no else '' + customer_doc.vc_road_no if  customer_doc.vc_road_no else '' + customer_doc.vc_city if customer_doc.vc_city else ''
+	mobile_no = customer_doc.mobile_number if customer_doc.mobile_number else ''
+	cpr_no = customer_doc.vc_cpr if customer_doc.vc_cpr else ''
+	email = customer_doc.email_id if customer_doc.email_id else ''
+	return {"address":address, "mobile_no":mobile_no, "cpr_no":cpr_no, "email":email}
