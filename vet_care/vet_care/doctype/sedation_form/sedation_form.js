c90ea4ba715d32f6e98f468de412180438722621
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2025-10-04 09:41:36 +0300

    print formats updated

--- vet_care/vet_care/doctype/sedation_form/sedation_form.js
+++ vet_care/vet_care/doctype/sedation_form/sedation_form.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, 9T9IT and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("SEDATION FORM", {
+frappe.ui.form.on("Sedation Form", {
 	refresh(frm) {
         frm.set_query('patient', () => {
             return {
