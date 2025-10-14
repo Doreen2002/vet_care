4abd0af4db39433c4bebdc0147cb1f4c16490f83
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2025-10-14 18:18:42 +0300

    remove employement type

--- vet_care/vet_care/doctype/employement_type/test_employement_type.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, 9T9IT and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestEmployementType(FrappeTestCase):
-	pass
