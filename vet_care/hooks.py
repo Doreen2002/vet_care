1753ad92edf25d778e282035e51a491966168a62
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-10-25 17:45:13 +0200

    config customer dashboard

--- vet_care/hooks.py
+++ vet_care/hooks.py
@@ -300,9 +300,9 @@ override_whitelisted_methods = {
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "vet_care.task.get_dashboard_data"
-# }
+override_doctype_dashboards = {
+	"Customer": "vet_care.config.customer_dashboard.get_data"
+}
 
 jenv = {
     "methods": [
