00cbcccafad9b4f9ae6c22dfd65f34dc43830592
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-10-27 08:35:02 +0200

    Add - copy + paste sales invoice no - pull items information on animal overview

--- vet_care/vet_care/doctype/animal_overview/animal_overview.js
+++ vet_care/vet_care/doctype/animal_overview/animal_overview.js
@@ -27,6 +27,9 @@ frappe.ui.form.on('Animal Overview', {
         });
     },
     refresh: function(frm) {
+      cur_frm.fields_dict.invoice.$input.on("keydown.invoice_evt", function(e) { 
+        frm.trigger('invoice');
+      });
         if (frappe.route_options && frappe.route_options.animal) {
             frm.set_value('animal', frappe.route_options.animal);
             frappe.route_options = {};
